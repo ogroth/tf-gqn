e29c4f31323fe27170ab891677153b13b754fb25
Ștefan Săftescu <stefan.saftescu@gmail.com>
2018-06-26 16:36:41 +0100

    Implement tower and pool representation networks.

--- gqn/gqn_graph.py
+++ gqn/gqn_graph.py
@@ -18,7 +18,55 @@ from __future__ import print_function
 import tensorflow as tf
 
 
-def gqn(inputs, scope="GQN"):
+def tower_encoder(images, poses, scope="TowerEncoder"):
+  with tf.variable_scope(scope):
+    endpoints = {}
+    net = tf.layers.conv2d(images, filters=256, kernel_size=2, strides=2,
+                           padding="VALID", activation=tf.nn.relu)
+    skip1 = tf.layers.conv2d(net, filters=128, kernel_size=1, strides=1,
+                             padding="SAME", activation=None)
+    net = tf.layers.conv2d(net, filters=128, kernel_size=3, strides=1,
+                           padding="SAME", activation=tf.nn.relu)
+    # TODO(ogroth): correct implementation for the skip connection?
+    net = net + skip1
+    net = tf.layers.conv2d(net, filters=256, kernel_size=2, strides=2,
+                           padding="VALID", activation=tf.nn.relu)
+
+    # tile the poses to match the embedding shape
+    with tf.control_dependencies([tf.assert_rank(poses, 2)]):  # (batch, 7)
+        height, width = tf.shape(net)[1], tf.shape(net)[2]
+
+        poses = tf.reshape(poses, [-1, 1, 1, 7])
+        poses = tf.tile(poses, [1, height, width, 1])
+
+    # concatenate the poses with the embedding
+    net = tf.concat([net, poses], axis=3)
+
+    skip2 = tf.layers.conv2d(net, filters=128, kernel_size=1, strides=1,
+                             padding="SAME", activation=None)
+    net = tf.layers.conv2d(net, filters=128, kernel_size=3, strides=1,
+                           padding="SAME", activation=tf.nn.relu)
+    # TODO(ogroth): correct implementation for the skip connection?
+    net = net + skip2
+
+    net = tf.layers.conv2d(net, filters=256, kernel_size=3, strides=1,
+                           padding="SAME", activation=tf.nn.relu)
+
+    net = tf.layers.conv2d(net, filters=256, kernel_size=1, strides=1,
+                           padding="SAME", activation=tf.nn.relu)
+
+    return net, endpoints
+
+
+def pool_encoder(images, poses, scope="PoolEncoder"):
+  net, endpoints = tower_encoder(images, poses, scope)
+  with tf.variable_scope(scope):
+      net = tf.reduce_mean(net, axis=[1, 2], keepdims=True)
+
+  return net, endpoints
+
+
+def gqn(images, poses, scope="GQN"):
   """
   Defines the computational graph of the GQN model.
 
@@ -27,7 +75,12 @@ def gqn(inputs, scope="GQN"):
     endpoints: A dictionary providing quick access to the most important model
       nodes in the computational graph.
   """
-  #TODO: implement
   endpoints = {}
-  net = inputs
+
+  representation, endpoints_r = tower_encoder(images, poses)
+
+  endpoints.update(endpoints_r)
+  endpoints["representation"] = representation
+
+  net = representation
   return net, endpoints

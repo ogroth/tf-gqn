cb3af4eb3bfdfe958a29a5af541671b972da0318
Oliver Groth <omgroth@gmail.com>
2018-07-02 18:38:01 +0100

    formatting

--- tests/test_gqn_graph.py
+++ tests/test_gqn_graph.py
@@ -10,14 +10,14 @@ from gqn.gqn_graph import gqn
 # graph definition
 img = tf.placeholder(shape=(1, 64, 64, 3), dtype=tf.float32)
 pose = tf.placeholder(shape=(1,7), dtype=tf.float32)
-representation, _ = gqn(img, pose)
+net, endpoints = gqn(img, pose)
 
 # feed random input through the graph
 with tf.Session() as sess:
   sess.run(tf.global_variables_initializer())
   a = sess.run(
-      representation,
+      net,
       feed_dict={
-          img: np.random.uniform(size=(1, 64, 64, 3)),
-          pose: np.random.uniform(size=(1, 7)),
+        img: np.random.uniform(size=(1, 64, 64, 3)),
+        pose: np.random.uniform(size=(1, 7)),
       })

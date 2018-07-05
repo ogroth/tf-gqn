8d5fdc647881a351dc0aa12b6e959447e3aaa7b5
Oliver Groth <omgroth@gmail.com>
2018-07-05 15:30:28 +0100

    included params

--- tests/test_gqn_graph.py
+++ tests/test_gqn_graph.py
@@ -5,11 +5,19 @@ Quick test script to check graph definition of GQN.
 import tensorflow as tf
 import numpy as np
 
+from gqn.gqn_params import PARAMS
 from gqn.gqn_graph import gqn
 
+# constants
+_BATCH_SIZE = 1
+_DIM_POSE = PARAMS.POSE_CHANNELS
+_DIM_H_IMG = PARAMS.IMG_HEIGHT
+_DIM_W_IMG = PARAMS.IMG_WIDTH
+_DIM_C_IMG = PARAMS.IMG_CHANNELS
+
 # graph definition
-img = tf.placeholder(shape=(1, 64, 64, 3), dtype=tf.float32)
-pose = tf.placeholder(shape=(1,7), dtype=tf.float32)
+img = tf.placeholder(shape=[_BATCH_SIZE, _DIM_H_IMG, _DIM_W_IMG, _DIM_C_IMG], dtype=tf.float32)
+pose = tf.placeholder(shape=[_BATCH_SIZE, _DIM_POSE], dtype=tf.float32)
 net, endpoints = gqn(img, pose)
 
 # feed random input through the graph
@@ -18,6 +26,6 @@ with tf.Session() as sess:
   a = sess.run(
       net,
       feed_dict={
-        img: np.random.uniform(size=(1, 64, 64, 3)),
-        pose: np.random.uniform(size=(1, 7)),
+          img: np.random.uniform(size=[_BATCH_SIZE, _DIM_H_IMG, _DIM_W_IMG, _DIM_C_IMG]),
+          pose: np.random.uniform(size=[_BATCH_SIZE, _DIM_POSE]),
       })

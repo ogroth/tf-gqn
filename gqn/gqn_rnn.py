84e8153df80ae68ae42a2d1c2f2a23a86c13d6fa
Ștefan Săftescu <stefan.saftescu@gmail.com>
2018-07-05 17:19:13 +0100

    Implement InferenceRNNCell, that can use interact with the GeneratorRNNCell at training time.

--- gqn/gqn_rnn.py
+++ gqn/gqn_rnn.py
@@ -2,7 +2,8 @@ import tensorflow as tf
 from collections import namedtuple
 
 from .gqn_params import PARAMS
-from .gqn_utils import broadcast_poses, eta, sample_z
+from .gqn_utils import broadcast_poses, sample_z
+
 
 class GQNLSTMCell(tf.contrib.rnn.RNNCell):
   # TODO(stefan): better description here
@@ -15,7 +16,8 @@ class GQNLSTMCell(tf.contrib.rnn.RNNCell):
                kernel_size=5,
                use_bias=True,
                forget_bias=1.0,
-               name="conv_lstm_cell"):
+               hidden_state_name="h",
+               name="GQNCell"):
     """Construct ConvLSTMCell.
     Args:
       conv_ndims: Convolution dimensionality (1, 2 or 3).
@@ -44,6 +46,7 @@ class GQNLSTMCell(tf.contrib.rnn.RNNCell):
     self._kernel_size = kernel_size
     self._use_bias = use_bias
     self._forget_bias = forget_bias
+    self._hidden_state_name = hidden_state_name
     state_size = tf.TensorShape(self._input_shape[:-1] + [output_channels])
     self._output_size = state_size
     self._state_size = tf.contrib.rnn.LSTMStateTuple(state_size, state_size)
@@ -65,7 +68,8 @@ class GQNLSTMCell(tf.contrib.rnn.RNNCell):
     """
     cell_state, hidden_state = state
 
-    new_hidden = self._conv(inputs + [hidden_state])
+    inputs[self._hidden_state_name] = hidden_state
+    new_hidden = self._conv(inputs)
     gates = tf.split(value=new_hidden,
                      num_or_size_splits=4,
                      axis=self._conv_ndims+1)
@@ -87,19 +91,21 @@ class GQNLSTMCell(tf.contrib.rnn.RNNCell):
     different cells when inputs match.
     """
     result = tf.zeros(shape=self._output_size.lstm)
-    for input in inputs:
-      result += tf.layers.conv2d(input,
+    for k in inputs:
+      result += tf.layers.conv2d(inputs[k],
                                  filters=4 * self._output_channels,
                                  kernel_size=self._kernel_size,
                                  strides=1,
                                  padding='SAME',
                                  use_bias=self._use_bias,
                                  activation=None,
-                                 name="{}_LSTMConv".format(input.name))
+                                 name="{}_LSTMConv".format(k))
 
     return result
 
 
+_GeneratorCellInput = namedtuple('GeneratorCellInput',
+                                 ['representation', 'query_pose', 'z'])
 _GeneratorCellOutput = namedtuple('GeneratorCellOutput', ['canvas', 'lstm'])
 _GeneratorCellState = namedtuple('GeneratorCellState', ['canvas', 'lstm'])
 
@@ -116,7 +122,7 @@ class GeneratorLSTMCell(tf.contrib.rnn.RNNCell):
                kernel_size=5,
                use_bias=True,
                forget_bias=1.0,
-               name="conv_lstm_cell"):
+               name="GeneratorLSTMCell"):
     """Construct ConvLSTMCell.
     Args:
       conv_ndims: Convolution dimensionality (1, 2 or 3).
@@ -140,7 +146,7 @@ class GeneratorLSTMCell(tf.contrib.rnn.RNNCell):
 
     self._gqn_cell = GQNLSTMCell(
       input_shape, output_channels, kernel_size, use_bias, forget_bias,
-      name="{}_GQNCell".format(name))
+      hidden_state_name="h_g", name="{}_GQNCell".format(name))
 
     # TODO(stefan,ogroth): do we want to hard-code here the output size of the
     #                      deconvolution to 4?
@@ -170,7 +176,7 @@ class GeneratorLSTMCell(tf.contrib.rnn.RNNCell):
     canvas, (cell_state, hidden_state) = state
     sub_state = tf.contrib.rnn.LSTMStateTuple(cell_state, hidden_state)
 
-    sub_output, new_sub_state = self._gqn_cell(inputs, sub_state)
+    sub_output, new_sub_state = self._gqn_cell(inputs._asdict(), sub_state)
 
     # upscale the output and add it to u (the side state)
     new_canvas = canvas + tf.layers.conv2d_transpose(
@@ -182,7 +188,90 @@ class GeneratorLSTMCell(tf.contrib.rnn.RNNCell):
     return new_output, new_state
 
 
-def generator_rnn(poses, representations, sequence_size=12,
+_InferenceCellInput = namedtuple('InferenceCellInput',
+                                 ['representation', 'query_pose', 'query_image',
+                                  'canvas', 'h_g'])
+
+
+class InferenceLSTMCell(tf.contrib.rnn.RNNCell):
+  # TODO(stefan): better description here
+  """InferenceLSTMCell wrapper that upscales output with a deconvolution and
+     adds it to a side input."""
+
+  def __init__(self,
+               input_shape,
+               output_channels,
+               kernel_size=5,
+               use_bias=True,
+               forget_bias=1.0,
+               name="InferenceLSTMCell"):
+    """Construct ConvLSTMCell.
+    Args:
+      conv_ndims: Convolution dimensionality (1, 2 or 3).
+      input_shape: Shape of the input as int tuple, excluding the batch size.
+      output_channels: int, number of output channels of the conv LSTM.
+      kernel_shape: Shape of kernel as in tuple (of size 1,2 or 3).
+      use_bias: (bool) Use bias in convolutions.
+      skip_connection: If set to `True`, concatenate the input to the
+        output of the conv LSTM. Default: `False`.
+      forget_bias: Forget bias.
+      initializers: Unused.
+      name: Name of the module.
+    Raises:
+      ValueError: If `skip_connection` is `True` and stride is different from 1
+        or if `input_shape` is incompatible with `conv_ndims`.
+    """
+    super(InferenceLSTMCell, self).__init__(name=name)
+
+    if len(input_shape) - 1 != 2:
+      raise ValueError("Invalid input_shape {}.".format(input_shape))
+
+    self._gqn_cell = GQNLSTMCell(
+      input_shape, output_channels, kernel_size, use_bias, forget_bias,
+      hidden_state_name='h_e', name="{}_GQNCell".format(name))
+
+    # TODO(stefan,ogroth): do we want to hard-code here the output size of the
+    #                      deconvolution to 4?
+    self._output_size = self._gqn_cell.output_size
+    self._state_size = self._gqn_cell.state_size
+
+  @property
+  def output_size(self):
+    return self._output_size
+
+  @property
+  def state_size(self):
+    return self._state_size
+
+  def call(self, inputs, state, scope=None):
+    """
+    :param inputs: concatenated pose, representation, and noise z
+    :param state: canvas (u), cell (c), and hidden (h) states
+    :param scope:
+    :return:
+    """
+    cell_state, hidden_state = state
+
+    input_dict = inputs._asdict()
+    query_image = input_dict.pop('query_image')
+    canvas = input_dict.pop('canvas')
+
+    # TODO(stefan,ogroth): actually, this could just be another input, what
+    #                      is the right thing to do?
+    # downscale the query image and canvas to add it to the hidden state
+    input_canvas_and_image = \
+      tf.layers.conv2d(tf.concat([query_image, canvas], axis=-1),
+                       filters=self.output_size[-1], kernel_size=4, strides=4,
+                       padding='VALID', use_bias=False)
+    hidden_state += input_canvas_and_image
+
+    state = tf.contrib.rnn.LSTMStateTuple(cell_state, hidden_state)
+    output, new_state = self._gqn_cell(input_dict, state)
+
+    return output, new_state
+
+
+def generator_rnn(representations, query_poses, sequence_size=12,
                   scope="GeneratorRNN"):
   batch = tf.shape(representations)[0]
   height, width = tf.shape(representations)[1], tf.shape(representations)[2]
@@ -200,23 +289,24 @@ def generator_rnn(poses, representations, sequence_size=12,
       if varscope.caching_device is None:
         varscope.set_caching_device(lambda op: op.device)
 
-    poses = broadcast_poses(poses, height, width)
-    inputs = tf.concat([poses, representations], axis=-1)
+    query_poses = broadcast_poses(query_poses, height, width)
     state = cell.zero(batch, tf.float32)
 
     for time in range(sequence_size):
       if time > 0:
         varscope.reuse_variables()
 
-      z = sample_z(state[1].h, scope="ita_pi")
-      (output, state) = cell(tf.concat([inputs, z], axis=-1), state)
+      z = sample_z(state[1].h, scope="eta_pi")
+
+      inputs = _GeneratorCellInput(representations, query_poses, z)
+      (output, state) = cell(inputs, state)
 
       outputs.append(output)
 
   return outputs[-1][0]
 
 
-def inference_rnn(poses, representations, sequence_size=12,
+def inference_rnn(representations, query_poses, query_images, sequence_size=12,
                   scope="InferenceRNN"):
 
   batch = tf.shape(representations)[0]
@@ -224,11 +314,6 @@ def inference_rnn(poses, representations, sequence_size=12,
 
   # TODO(stefan,ogroth): how are variables shared between inference and
   #                      generator?
-  inference_cell = GQNLSTMCell(
-    [height, width, PARAMS.INFERENCE_INPUT_CHANNELS],
-    PARAMS.LSTM_OUTPUT_CHANNELS,
-    PARAMS.LSTM_KERNEL_SIZE,
-    name="Cell")
 
   generator_cell = GeneratorLSTMCell(
     [height, width, PARAMS.GENERATOR_INPUT_CHANNELS],
@@ -236,14 +321,19 @@ def inference_rnn(poses, representations, sequence_size=12,
     PARAMS.LSTM_CANVAS_CHANNELS,
     PARAMS.LSTM_KERNEL_SIZE, name="Cell")
 
+  inference_cell = InferenceLSTMCell(
+    [height, width, PARAMS.INFERENCE_INPUT_CHANNELS],
+    PARAMS.LSTM_OUTPUT_CHANNELS,
+    PARAMS.LSTM_KERNEL_SIZE,
+    name="Cell")
+
   outputs = []
   with tf.variable_scope(scope) as varscope:
     if not tf.executing_eagerly():
       if varscope.caching_device is None:
         varscope.set_caching_device(lambda op: op.device)
 
-    poses = broadcast_poses(poses, height, width)
-    inputs = tf.concat([poses, representations], axis=-1)
+    query_poses = broadcast_poses(query_poses, height, width)
 
     inf_state = inference_cell.zero_state(batch, tf.float32)
     gen_state = generator_cell.zero(batch, tf.float32)
@@ -252,16 +342,18 @@ def inference_rnn(poses, representations, sequence_size=12,
       if time > 0:
         varscope.reuse_variables()
 
-      # TODO(stefan,ogroth): we need x^q, u^l
-      inf_input = inputs
+      inf_input = _InferenceCellInput(
+        representations, query_poses, query_images, gen_state.canvas,
+        gen_state.lstm.h)
 
       z = sample_z(inf_state.h, scope="ita_q")
-      gen_input = tf.concat([inputs, z], axis=-1)
+      gen_input = _GeneratorCellInput(representations, query_poses, z)
 
       # TODO(stefan,ogroth): how do you actually use the inference hidden state?
       inf_state[1].h += gen_state[1].h
 
       (inf_output, inf_state) = inference_cell(inf_input, inf_state)
+      varscope.reuse_variables()
       (gen_output, gen_state) = generator_cell(gen_input, gen_state)
 
       outputs.append((inf_output, gen_output))

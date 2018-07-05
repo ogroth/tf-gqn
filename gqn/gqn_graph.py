dd63fbe1f86a8a5e0c40a1f19e631f3a4de704d6
Ștefan Săftescu <stefan.saftescu@gmail.com>
2018-07-05 14:13:19 +0100

    Implement custom ConvLSTMCell to allow for variable sharing between generator and inference RNNs.

--- gqn/gqn_graph.py
+++ gqn/gqn_graph.py
@@ -16,6 +16,7 @@ from __future__ import division
 from __future__ import print_function
 
 import tensorflow as tf
+from collections import namedtuple
 
 
 # constants
@@ -108,6 +109,100 @@ def _sample_z(h, kernel_size=5, scope=None):
     return z
 
 
+class GQNLSTMCell(tf.contrib.rnn.RNNCell):
+  # TODO(stefan): better description here
+  """GeneratorLSTMCell wrapper that upscales output with a deconvolution and
+     adds it to a side input."""
+
+  def __init__(self,
+               input_shape,
+               output_channels,
+               kernel_size=5,
+               use_bias=True,
+               forget_bias=1.0,
+               name="conv_lstm_cell"):
+    """Construct ConvLSTMCell.
+    Args:
+      conv_ndims: Convolution dimensionality (1, 2 or 3).
+      input_shape: Shape of the input as int tuple, excluding the batch size.
+      output_channels: int, number of output channels of the conv LSTM.
+      kernel_shape: Shape of kernel as in tuple (of size 1,2 or 3).
+      use_bias: (bool) Use bias in convolutions.
+      skip_connection: If set to `True`, concatenate the input to the
+        output of the conv LSTM. Default: `False`.
+      forget_bias: Forget bias.
+      initializers: Unused.
+      name: Name of the module.
+    Raises:
+      ValueError: If `skip_connection` is `True` and stride is different from 1
+        or if `input_shape` is incompatible with `conv_ndims`.
+    """
+    super(GQNLSTMCell, self).__init__(name=name)
+
+    if len(input_shape) - 1 != 2:
+      raise ValueError("Invalid input_shape {}.".format(input_shape))
+
+    # TODO(stefan,ogroth): do we want to hard-code here the output size of the
+    #                      deconvolution to 4?
+    self._input_shape = input_shape
+    self._output_channels = output_channels
+    self._kernel_size = kernel_size
+    self._use_bias = use_bias
+    self._forget_bias = forget_bias
+    state_size = tf.TensorShape(self._input_shape[:-1] + [output_channels])
+    self._output_size = state_size
+    self._state_size = tf.contrib.rnn.LSTMStateTuple(state_size, state_size)
+
+  @property
+  def output_size(self):
+    return self._output_size
+
+  @property
+  def state_size(self):
+    return self._state_size
+
+  def call(self, inputs, state, scope=None):
+    """
+    :param inputs: list of inputs
+    :param state: cell (c), and hidden (h) states
+    :param scope:
+    :return:
+    """
+    cell_state, hidden_state = state
+
+    new_hidden = self._conv(inputs + [hidden_state])
+    gates = tf.split(value=new_hidden,
+                     num_or_size_splits=4,
+                     axis=self._conv_ndims+1)
+
+    input_gate, new_input, forget_gate, output_gate = gates
+    new_cell = tf.nn.sigmoid(forget_gate + self._forget_bias) * cell_state
+    new_cell += tf.nn.sigmoid(input_gate) * tf.nn.tanh(new_input)
+
+    output = tf.nn.tanh(new_cell) * tf.nn.sigmoid(output_gate)
+    new_state = tf.contrib.rnn.LSTMStateTuple(new_cell, output)
+
+    return output, new_state
+
+  def _conv(self, inputs):
+    result = tf.zeros(shape=self._output_size.lstm)
+    for input in inputs:
+      result += tf.layers.conv2d(input,
+                                 filters=4 * self._output_channels,
+                                 kernel_size=self._kernel_size,
+                                 strides=1,
+                                 padding='SAME',
+                                 use_bias=self._use_bias,
+                                 activation=None,
+                                 name="{}_LSTMConv".format(input.name))
+
+    return result
+
+
+_GeneratorCellOutput = namedtuple('GeneratorCellOutput', ['canvas', 'lstm'])
+_GeneratorCellState = namedtuple('GeneratorCellState', ['canvas', 'lstm'])
+
+
 class GeneratorLSTMCell(tf.contrib.rnn.RNNCell):
   # TODO(stefan): better description here
   """GeneratorLSTMCell wrapper that upscales output with a deconvolution and
@@ -120,7 +215,6 @@ class GeneratorLSTMCell(tf.contrib.rnn.RNNCell):
                kernel_size=5,
                use_bias=True,
                forget_bias=1.0,
-               initializers=None,
                name="conv_lstm_cell"):
     """Construct ConvLSTMCell.
     Args:
@@ -143,22 +237,19 @@ class GeneratorLSTMCell(tf.contrib.rnn.RNNCell):
     if len(input_shape) - 1 != 2:
       raise ValueError("Invalid input_shape {}.".format(input_shape))
 
-    self._conv_cell = tf.contrib.rnn.Conv2DLSTMCell(
-      input_shape=input_shape,
-      output_channels=output_channels,
-      kernel_shape=[kernel_size, kernel_size],
-      use_bias=use_bias,
-      forget_bias=forget_bias,
-      initializers=initializers,
-      name=name + "_ConvLSTMCell")
+    self._gqn_cell = GQNLSTMCell(
+      input_shape, output_channels, kernel_size, use_bias, forget_bias,
+      name="{}_GQNCell".format(name))
 
     # TODO(stefan,ogroth): do we want to hard-code here the output size of the
     #                      deconvolution to 4?
     canvas_size = tf.TensorShape(
       map(lambda x: 4 * x, input_shape[:-1]) + [canvas_channels])
-    self._side_state_channels = canvas_channels
-    self._output_size = (canvas_size, self._conv_cell.output_size)
-    self._state_size = (canvas_size, self._conv_cell.state_size)
+    self._canvas_channels = canvas_channels
+    self._output_size = _GeneratorCellOutput(canvas_size,
+                                             self._gqn_cell.output_size)
+    self._state_size = _GeneratorCellState(canvas_size,
+                                           self._gqn_cell.state_size)
 
   @property
   def output_size(self):
@@ -178,8 +269,7 @@ class GeneratorLSTMCell(tf.contrib.rnn.RNNCell):
     canvas, (cell_state, hidden_state) = state
     sub_state = tf.contrib.rnn.LSTMStateTuple(cell_state, hidden_state)
 
-    # run the ConvLSTMCell
-    sub_output, new_sub_state = self._conv_cell(inputs, sub_state)
+    sub_output, new_sub_state = self._gqn_cell(inputs, sub_state)
 
     # upscale the output and add it to u (the side state)
     new_canvas = canvas + tf.layers.conv2d_transpose(
@@ -232,11 +322,11 @@ def inference_rnn(poses, representations, sequence_size=12,
   #                      generator?
   inference_cell = tf.contrib.rnn.Conv2DLSTMCell(
     [height, width, _INFERENCE_INPUT_CHANNELS], _LSTM_OUTPUT_CHANNELS,
-    [_LSTM_KERNEL_SIZE, _LSTM_KERNEL_SIZE], name="InferenceCell")
+    [_LSTM_KERNEL_SIZE, _LSTM_KERNEL_SIZE], name="Cell")
 
   generator_cell = GeneratorLSTMCell(
     [height, width, _GENERATOR_INPUT_CHANNELS], _LSTM_OUTPUT_CHANNELS,
-    _LSTM_CANVAS_CHANNELS, _LSTM_KERNEL_SIZE, name="GeneratorCell")
+    _LSTM_CANVAS_CHANNELS, _LSTM_KERNEL_SIZE, name="Cell")
 
   outputs = []
   with tf.variable_scope(scope) as varscope:

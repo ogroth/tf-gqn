045a75e0dafd16f612b24acebcebb24496dd29ed
Oliver Groth <omgroth@gmail.com>
2018-07-09 15:21:10 +0100

    [WIP] complete GQN setup

--- gqn/gqn_graph.py
+++ gqn/gqn_graph.py
@@ -19,13 +19,15 @@ import tensorflow as tf
 
 from .gqn_params import _GQNParams
 from .gqn_encoder import pool_encoder
+from .gqn_rnn import inference_rnn, generator_rnn
 
 
 
 def gqn(
     query_pose: tf.Tensor, target_frame: tf.Tensor,
     context_poses: tf.Tensor, context_frames: tf.Tensor,
-    model_params: _GQNParams, scope: str = "GQN"):
+    model_params: _GQNParams, is_training: bool = True,
+    scope: str = "GQN"):
   """
   Defines the computational graph of the GQN model.
 
@@ -36,6 +38,9 @@ def gqn(
     context_frames: Frames of the context views.
     model_params: Named tuple containing the parameters of the GQN model as \
       defined in gqn_params.py
+    is_training: Flag whether graph shall be created in training mode (including \
+      the inference module necessary for training the generator). If set to 'False',
+      only the generator LSTM will be created.
     scope: Scope name of the graph.
 
   Returns:
@@ -53,6 +58,7 @@ def gqn(
   _DIM_H_ENC = model_params.ENC_HEIGHT
   _DIM_W_ENC = model_params.ENC_WIDTH
   _DIM_C_ENC = model_params.ENC_CHANNELS
+  _SEQ_LENGTH = model_params.SEQ_LENGTH
 
   with tf.variable_scope(scope):
     endpoints = {}
@@ -63,16 +69,39 @@ def gqn(
         context_frames, shape=[-1, _DIM_H_IMG, _DIM_W_IMG, _DIM_C_IMG])
 
     # define scene encoding graph psi
-    enc_r_packed, endpoints_psi = pool_encoder(context_frames, context_poses)
+    enc_r_packed, endpoints_psi = pool_encoder(context_frames_packed, context_poses_packed)
+    endpoints.update(endpoints_psi)
 
     # unpack scene encoding and reduce to single vector
     enc_r_packed = tf.reshape(
         enc_r_packed,
         shape=[_BATCH_SIZE, _CONTEXT_SIZE, _DIM_H_ENC, _DIM_H_ENC, _DIM_C_ENC])
     enc_r = tf.reduce_sum(enc_r_packed, axis=1) # add scene representations per data tuple
-
-    endpoints.update(endpoints_psi)
     endpoints["enc_r"] = enc_r
+
+    # define generator graph (with inference component if in training mode)
+    if is_training:
+      # outputs = [(inf_output, gen_output)]
+      # inf_output: lstm -> h^e_l
+      # gen_output: canvas -> u_l, lstm -> h^g_l
+      outputs = inference_rnn( # TODO(ogroth): return endpoints to eta functions!
+          representations=enc_r,
+          query_poses=query_pose,
+          target_frames=target_frame,
+          sequence_size=_SEQ_LENGTH,
+      )
+      # expose endpoints for loss computation: (mu, sigma) tensors
+      # for all h^e_l: eta^q_psi, eta^pi_theta -> (mu, sigma)
+      for i, t in enumerate(outputs):
+        inf_mu = "inf_mu_%d" % (i, )
+        gen_mu = "gen_mu_%d" % (i, )
+        inf_sigma = "inf_sigma_%d" % (i, )
+        gen_sigma = "gen_sigma_%d" % (i, )
+        canvas = "canvas_%d" % (i, )
+      # return mus to sample target image
+      # u_L: eta^g_theta -> mu
+    else:
+      pass
 
     net = enc_r
     return net, endpoints

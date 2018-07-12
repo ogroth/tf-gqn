a998cb1be4a1cc990f4d369f7827ee3b7877917c
Oliver Groth <omgroth@gmail.com>
2018-07-12 14:03:54 +0100

    [WIP] estimator model_fn

--- gqn/gqn_model.py
+++ gqn/gqn_model.py
@@ -18,10 +18,11 @@ from __future__ import print_function
 import tensorflow as tf
 
 from .gqn_graph import gqn
+from gqn.gqn_objective import gqn_elbo
 from .gqn_params import _GQNParams
 
 
-def _linear_annealing_scheme(gqn_params: _GQNParams):
+def _linear_annealing_scheme(gqn_params: _GQNParams) -> tf.Tensor:
   """
   Defines the computational graph for the global sigma annealing scheme used in
   image sampling.
@@ -52,6 +53,9 @@ def gqn_model_fn(features, labels, mode, params):
   Returns:
     spec: tf.estimator.EstimatorSpec
   """
+  # shorthand notations for parameters
+  _SEQ_LENGTH = params['gqn_params'].SEQ_LENGTH
+
   # feature and label mapping according to gqn_input_fn
   query_pose = features.query.query_camera
   target_frame = labels
@@ -68,21 +72,70 @@ def gqn_model_fn(features, labels, mode, params):
       is_training=(mode == tf.estimator.ModeKeys.TRAIN)
   )
 
+  # collect intermediate endpoints
+  mu_q, sigma_q, mu_pi, sigma_pi = [], [], [], []
+  for i in range(_SEQ_LENGTH):
+    mu_q.append(ep_gqn["mu_q_%d" % i])
+    sigma_q.append(ep_gqn["sigma_q_%d" % i])
+    mu_pi.append(ep_gqn["mu_pi_%d" % i])
+    sigma_pi.append(ep_gqn["sigma_pi_%d" % i])
+
   # outputs: sampled images
   mu_target = net
   sigma_target = _linear_annealing_scheme(params['gqn_params'])
-  target_sample = tf.constant(42)  # stub
+  target_normal = tf.distributions.Normal(loc=mu_target, scale=sigma_target)
+  target_sample = target_normal.sample()
   # image generation steps in debug mode
+  # TODO(ogroth): add sampling from intermediate endpoints
 
-  # predictions
-  predictions = {
-    'target_sample' : target_sample
-  }
+  # predictions to make when deployed during test time
+  if mode == tf.estimator.ModeKeys.PREDICT:
+    predictions = {
+        'target_sample' : target_sample
+    }
 
   # ELBO setup
-  # eval_metric_ops
-  # train & eval summary hooks
-  # optimizer
-  # train_op
+  if mode != tf.estimator.ModeKeys.PREDICT:
+    elbo = gqn_elbo(
+        mu_target, sigma_target,
+        mu_q, sigma_q,
+        mu_pi, sigma_pi,
+        target_frame)
 
-  raise NotImplementedError()
+  # optimization
+  if mode == tf.estimator.ModeKeys.TRAIN:
+    # TODO(ogroth): tune hyper-parameters of optimizer?
+    optimizer = tf.train.AdamOptimizer()
+    train_op = optimizer.minimize(
+        loss=elbo,
+        global_step=tf.train.get_global_step()
+    )
+
+  # evaluation metrics to monitor
+  if mode == tf.estimator.ModeKeys.EVAL:
+    eval_metric_ops = {
+        'mean_abs_pixel_error' : tf.metrics.mean_absolute_error(
+            labels=target_frame,
+            predictions=target_sample)
+    }
+
+  # train & eval summary hooks
+  # TODO(ogroth): add image summaries for intermediate images
+
+  # create SpecSheet
+  if mode == tf.estimator.ModeKeys.TRAIN:
+    estimator_spec = tf.estimator.EstimatorSpec(
+      mode=mode,
+      loss=elbo,
+      train_op=train_op)
+  if mode == tf.estimator.ModeKeys.EVAL:
+    estimator_spec = tf.estimator.EstimatorSpec(
+      mode=mode,
+      loss=elbo,
+      eval_metric_ops=eval_metric_ops)
+  if mode == tf.estimator.ModeKeys.PREDICT:
+    estimator_spec = tf.estimator.EstimatorSpec(
+      mode=mode,
+      predictions=predictions)
+
+  return estimator_spec

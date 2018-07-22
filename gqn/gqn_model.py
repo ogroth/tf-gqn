f367141ea4de6a142da2429752537f7edb55926b
Oliver Groth <omgroth@gmail.com>
2018-07-22 13:33:20 +0100

    added debugging model for indentity learning

--- gqn/gqn_model.py
+++ gqn/gqn_model.py
@@ -19,7 +19,7 @@ import tensorflow as tf
 
 from .gqn_graph import gqn_draw, gqn_vae
 from .gqn_objective import gqn_draw_elbo, gqn_vae_elbo
-from .gqn_params import _GQNParams
+from .gqn_params import _GQNParams, _DEFAULTS
 from .gqn_utils import debug_canvas_image_mean
 
 
@@ -154,6 +154,149 @@ def gqn_draw_model_fn(features, labels, mode, params):
   # optimization
   if mode == tf.estimator.ModeKeys.TRAIN:
     lr = _linear_lr_annealing(params['gqn_params'])
+    optimizer = tf.train.AdamOptimizer(learning_rate=lr)
+    train_op = optimizer.minimize(
+        loss=elbo,
+        global_step=tf.train.get_global_step()
+    )
+
+  # evaluation metrics to monitor
+  if mode == tf.estimator.ModeKeys.EVAL:
+    eval_metric_ops = {
+        'mean_abs_pixel_error' : tf.metrics.mean_absolute_error(
+            labels=target_frame,
+            predictions=mu_target)
+    }
+
+  # create SpecSheet
+  if mode == tf.estimator.ModeKeys.TRAIN:
+    estimator_spec = tf.estimator.EstimatorSpec(
+        mode=mode,
+        loss=elbo,
+        train_op=train_op)
+  if mode == tf.estimator.ModeKeys.EVAL:
+    estimator_spec = tf.estimator.EstimatorSpec(
+        mode=mode,
+        loss=elbo,
+        eval_metric_ops=eval_metric_ops)
+  if mode == tf.estimator.ModeKeys.PREDICT:
+    estimator_spec = tf.estimator.EstimatorSpec(
+        mode=mode,
+        predictions=predictions)
+
+  return estimator_spec
+
+
+def gqn_draw_identity_model_fn(features, labels, mode, params):
+  """
+  Defines an tf.estimator.EstimatorSpec for the GQN model.
+  Debug version of the model function learning an identity between input and
+  output image.
+
+  Args:
+    features: Query = collections.namedtuple('Query', ['context', 'query_camera'])
+    labels: tf.Tensor of the target image
+    mode:
+    params:
+      gqn_params: _GQNParams type containing the model parameters
+      debug: bool; if true, model will produce additional debug output
+        tensorboard summaries for image generation process
+
+  Returns:
+    spec: tf.estimator.EstimatorSpec
+  """
+
+  # customize parameters
+  custom_params_dict = _DEFAULTS
+  custom_params_dict['CONTEXT_SIZE'] = 1
+  custom_params_dict['SEQ_LENGTH'] = 1
+  custom_params = _GQNParams(**custom_params_dict)
+
+  # shorthand notations for parameters
+  _CONTEXT_SIZE = custom_params.CONTEXT_SIZE
+  _SEQ_LENGTH = custom_params.SEQ_LENGTH
+
+  # feature and label mapping according to gqn_input_fn
+  query_pose = features.query_camera
+  target_frame = labels
+  context_poses = [features.query_camera]
+  context_frames = [labels]
+
+  # graph setup
+  net, ep_gqn = gqn_draw(
+      query_pose=query_pose,
+      target_frame=target_frame,
+      context_poses=context_poses,
+      context_frames=context_frames,
+      model_params=custom_params,
+      is_training=(mode != tf.estimator.ModeKeys.PREDICT)
+  )
+
+  # outputs: sampled images
+  mu_target = net
+  sigma_target = _linear_noise_annealing(custom_params)
+  target_normal = tf.distributions.Normal(loc=mu_target, scale=sigma_target)
+  target_sample = tf.identity(target_normal.sample(), name='target_sample')
+  # write out image summaries in debug mode
+  if params['debug']:
+    for i in range(_CONTEXT_SIZE):
+      tf.summary.image(
+          'context_frame_%d' % (i + 1),
+          context_frames[:, i],
+          max_outputs=1
+      )
+    tf.summary.image(
+        'target_images',
+        labels,
+        max_outputs=1
+    )
+    tf.summary.image(
+        'target_means',
+        mu_target,
+        max_outputs=1
+    )
+    generator_sequence = debug_canvas_image_mean(
+        [ep_gqn['canvas_{}'.format(i)] for i in range(_SEQ_LENGTH)]
+    )
+    tf.summary.image(
+        'generator_sequence_mean',
+        generator_sequence,
+        max_outputs=1
+    )
+
+  # predictions to make when deployed during test time
+  if mode == tf.estimator.ModeKeys.PREDICT:
+    predictions = {
+        'target_sample' : target_sample
+    }
+
+  # ELBO setup
+  if mode != tf.estimator.ModeKeys.PREDICT:
+    # collect intermediate endpoints
+    mu_q, sigma_q, mu_pi, sigma_pi = [], [], [], []
+    for i in range(_SEQ_LENGTH):
+      mu_q.append(ep_gqn["mu_q_%d" % i])
+      sigma_q.append(ep_gqn["sigma_q_%d" % i])
+      mu_pi.append(ep_gqn["mu_pi_%d" % i])
+      sigma_pi.append(ep_gqn["sigma_pi_%d" % i])
+    elbo, ep_elbo = gqn_draw_elbo(
+        mu_target, sigma_target,
+        mu_q, sigma_q,
+        mu_pi, sigma_pi,
+        target_frame)
+    if params['debug']:
+      tf.summary.scalar(
+          name='target_llh',
+          tensor=ep_elbo['target_llh']
+      )
+      tf.summary.scalar(
+          name='kl_regularizer',
+          tensor=ep_elbo['kl_regularizer']
+      )
+
+  # optimization
+  if mode == tf.estimator.ModeKeys.TRAIN:
+    lr = _linear_lr_annealing(custom_params)
     optimizer = tf.train.AdamOptimizer(learning_rate=lr)
     train_op = optimizer.minimize(
         loss=elbo,
